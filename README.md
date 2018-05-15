14533f1e68984d24739043e0ff77a08e0a9f915f
Peter Scopes <peter.scopes@gmail.com>
2018-05-15 13:41:06 +0100

    Improved dev.php and updated README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# php-helpers
+# madesimple/php-helpers
 Small helper functions for different situations
+
+## How to use
+Add a helper file to your `autoload-dev` for dev environments only:
+```json
+{
+  "autoload-dev": {
+    "files": ["vendor/madesimple/php-helpers/dev.php"]
+  }
+}
+```
+
+Add a helper file to your `autoload` for all environments:
+```json
+{
+  "autoload": {
+    "files": ["vendor/madesimple/php-helpers/helpers.php"]
+  }
+}
+```
---
+++ dev.php
@@ -0,0 +1,46 @@
+<?php
+
+if (!function_exists('dd')) {
+    /**
+     * Dump the passed variables and end the script.
+     *
+     * @param mixed
+     * @return void
+     */
+    function dd() {
+        $args = func_get_args() ?? [];
+
+        $stack  = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $format = php_sapi_name() === 'cli' ? '%s:%s' : '<pre><strong>%s:%s</strong>';
+
+        echo sprintf($format, $stack[0]['file'], $stack[0]['line']), PHP_EOL;
+
+        if (!empty($args)) {
+            call_user_func_array('var_dump', $args);
+        }
+        die(1);
+    }
+}
+if (!function_exists('vd')) {
+    /**
+     * Dump the passed variables.
+     *
+     * @param mixed
+     * @return void
+     */
+    function vd() {
+        $args = func_get_args() ?? [];
+
+        $stack  = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $format = php_sapi_name() === 'cli' ? '%s:%s' : '<pre><strong>%s:%s</strong>';
+
+        echo sprintf($format, $stack[0]['file'], $stack[0]['line']), PHP_EOL;
+
+        if (!empty($args)) {
+            call_user_func_array('var_dump', $args);
+        }
+    }
+}
+
+
+dd('hello');
